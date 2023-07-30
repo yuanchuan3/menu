ff6323d2de94f83d7d9b6eb544847125e466c5c8
somebody <nobody1@somebodys-MacBook-Air.local>
2023-07-30 10:40:29 +0800

    sync

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  s + "/_app/immutable/entry/app.e24ac50a.js",
+  s + "/_app/immutable/entry/app.401e10aa.js",
   s + "/_app/immutable/assets/0.731183ec.css",
   s + "/_app/immutable/nodes/0.c72192f2.js",
-  s + "/_app/immutable/nodes/1.43eae8d8.js",
+  s + "/_app/immutable/nodes/1.fe59cbf7.js",
   s + "/_app/immutable/assets/2.0ec1ad69.css",
-  s + "/_app/immutable/nodes/2.b92a3846.js",
+  s + "/_app/immutable/nodes/2.ce646fd0.js",
   s + "/_app/immutable/chunks/index.3a043c0e.js",
   s + "/_app/immutable/chunks/scheduler.c12c5fa2.js",
-  s + "/_app/immutable/chunks/singletons.740254e0.js",
-  s + "/_app/immutable/entry/start.3e5380f4.js"
+  s + "/_app/immutable/chunks/singletons.d5e04f8f.js",
+  s + "/_app/immutable/entry/start.443dec4d.js"
 ], p = [
   s + "/.nojekyll",
   s + "/favicon.svg",
   s + "/manifest.json"
-], i = "1690683998701", o = `cache${i}`, l = r.concat(p), u = new Set(l);
+], i = "1690684818069", o = `cache${i}`, l = r.concat(p), f = new Set(l);
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open(o).then((t) => t.addAll(l)).then(() => {
@@ -30,7 +30,7 @@ self.addEventListener("activate", (e) => {
     })
   );
 });
-async function m(e) {
+async function u(e) {
   const t = await caches.open(`offline${i}`);
   try {
     const a = await fetch(e);
@@ -45,8 +45,8 @@ async function m(e) {
 self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET" || e.request.headers.has("range"))
     return;
-  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, n = t.host === self.location.host && u.has(t.pathname), h = e.request.cache === "only-if-cached" && !n;
+  const t = new URL(e.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, n = t.host === self.location.host && f.has(t.pathname), h = e.request.cache === "only-if-cached" && !n;
   String(e.request.url), a && !c && !h && e.respondWith(
-    (async () => n && await caches.match(e.request) || m(e.request))()
+    (async () => n && await caches.match(e.request) || u(e.request))()
   );
 });
