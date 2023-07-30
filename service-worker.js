16d63ec1dad68530ebcfa9a99a38eb911a7591a4
somebody <nobody1@somebodys-MacBook-Air.local>
2023-07-30 12:11:50 +0800

    sync

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  e + "/_app/immutable/entry/app.1627fd2b.js",
+  e + "/_app/immutable/entry/app.1fe3748c.js",
   e + "/_app/immutable/assets/0.731183ec.css",
   e + "/_app/immutable/nodes/0.c72192f2.js",
-  e + "/_app/immutable/nodes/1.52200355.js",
-  e + "/_app/immutable/assets/2.0ec1ad69.css",
-  e + "/_app/immutable/nodes/2.ce646fd0.js",
+  e + "/_app/immutable/nodes/1.e0e823d0.js",
+  e + "/_app/immutable/assets/2.7950da74.css",
+  e + "/_app/immutable/nodes/2.35a5fb3c.js",
   e + "/_app/immutable/chunks/index.3a043c0e.js",
   e + "/_app/immutable/chunks/scheduler.c12c5fa2.js",
-  e + "/_app/immutable/chunks/singletons.57614f8a.js",
-  e + "/_app/immutable/entry/start.e60dd8e8.js"
+  e + "/_app/immutable/chunks/singletons.0e13010d.js",
+  e + "/_app/immutable/entry/start.d2186f67.js"
 ], p = [
   e + "/.nojekyll",
   e + "/favicon.svg",
   e + "/manifest.json"
-], i = "1690687093636", o = `cache${i}`, l = r.concat(p), u = new Set(l);
+], i = "1690690294349", o = `cache${i}`, l = r.concat(p), u = new Set(l);
 self.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(o).then((t) => t.addAll(l)).then(() => {
