7654fe5048849c1217a607288b5cc399a08561f1
somebody <nobody1@somebodys-MacBook-Air.local>
2023-07-31 01:52:07 +0800

    sync

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), r = [
-  e + "/_app/immutable/entry/app.c3a66c86.js",
+  e + "/_app/immutable/entry/app.e311a908.js",
   e + "/_app/immutable/assets/0.731183ec.css",
   e + "/_app/immutable/nodes/0.c72192f2.js",
-  e + "/_app/immutable/nodes/1.ec7128b7.js",
+  e + "/_app/immutable/nodes/1.b257a36e.js",
   e + "/_app/immutable/assets/2.7950da74.css",
-  e + "/_app/immutable/nodes/2.09a972ee.js",
+  e + "/_app/immutable/nodes/2.f69b3ffb.js",
   e + "/_app/immutable/chunks/index.3a043c0e.js",
   e + "/_app/immutable/chunks/scheduler.c12c5fa2.js",
-  e + "/_app/immutable/chunks/singletons.a9a89b7f.js",
-  e + "/_app/immutable/entry/start.6ab64961.js"
+  e + "/_app/immutable/chunks/singletons.92a70e5b.js",
+  e + "/_app/immutable/entry/start.824d6e5c.js"
 ], p = [
   e + "/.nojekyll",
   e + "/favicon.svg",
   e + "/manifest.json"
-], i = "1690737872186", o = `cache${i}`, l = r.concat(p), u = new Set(l);
+], i = "1690739518902", o = `cache${i}`, l = r.concat(p), u = new Set(l);
 self.addEventListener("install", (s) => {
   s.waitUntil(
     caches.open(o).then((t) => t.addAll(l)).then(() => {
@@ -30,7 +30,7 @@ self.addEventListener("activate", (s) => {
     })
   );
 });
-async function m(s) {
+async function f(s) {
   const t = await caches.open(`offline${i}`);
   try {
     const a = await fetch(s);
@@ -47,6 +47,6 @@ self.addEventListener("fetch", (s) => {
     return;
   const t = new URL(s.request.url), a = t.protocol.startsWith("http"), c = t.hostname === self.location.hostname && t.port !== self.location.port, n = t.host === self.location.host && u.has(t.pathname), h = s.request.cache === "only-if-cached" && !n;
   String(s.request.url), a && !c && !h && s.respondWith(
-    (async () => n && await caches.match(s.request) || m(s.request))()
+    (async () => n && await caches.match(s.request) || f(s.request))()
   );
 });
